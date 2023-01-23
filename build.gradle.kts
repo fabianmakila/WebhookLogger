0d8aa8a9a9ba24745fefef538a934fa4c96daf48
fabianmakila <contact@fabianadrian.fi>
2023-01-23 22:39:07 +0200

    Change supported api version to 1.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,48 @@
 plugins {
     id("java")
+    id("net.kyori.indra") version "3.0.1"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
 }
 
 group = "fi.fabianadrian"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
+description = "A simple plugin that forwards chat messages to a webhook."
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+
+    implementation("club.minnced:discord-webhooks:0.8.2")
+    implementation("org.spongepowered:configurate-hocon:4.1.2")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+indra {
+    javaVersions().target(17)
+}
+
+tasks {
+    shadowJar {
+        minimize()
+        sequenceOf(
+            "club.minnced.discord.webhook",
+            "org.spongepowered.configurate"
+        ).forEach { pkg ->
+            relocate(pkg, "${group}.${rootProject.name.toLowerCase()}.lib.$pkg")
+        }
+    }
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+bukkit {
+    main = "fi.fabianadrian.webhookchatlogger.WebhookChatLogger"
+    apiVersion = "1.17"
+    authors = listOf("FabianAdrian")
+    website = "https://github.com/fabianmakila/WebhookChatLogger"
 }
