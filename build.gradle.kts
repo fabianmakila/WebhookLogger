65564f0dd1507e32ce84e44f0043bacd6c286a85
fabianmakila <contact@fabianadrian.fi>
2023-03-26 21:23:12 +0300

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("java")
     id("net.kyori.indra") version "3.0.1"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
 }
 
 group = "fi.fabianadrian"
