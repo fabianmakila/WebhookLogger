1cdfebb7606689ae92331343f7afd94875107625
fabianmakila <contact@fabianadrian.fi>
2023-04-21 14:07:51 +0300

    feat: add comments to configuration options

--- src/main/java/fi/fabianadrian/webhookchatlogger/config/WebhookChatLoggerConfig.java
+++ src/main/java/fi/fabianadrian/webhookchatlogger/config/WebhookChatLoggerConfig.java
@@ -1,32 +1,51 @@
 package fi.fabianadrian.webhookchatlogger.config;
 
 import fi.fabianadrian.webhookchatlogger.client.ClientType;
+import space.arim.dazzleconf.annote.ConfComments;
 import space.arim.dazzleconf.annote.ConfDefault;
+import space.arim.dazzleconf.annote.ConfKey;
 import space.arim.dazzleconf.annote.SubSection;
 
 
 public interface WebhookChatLoggerConfig {
     @ConfDefault.DefaultString("")
+    @ConfComments("Webhook URL.")
     String url();
 
     @ConfDefault.DefaultString("DISCORD")
-    ClientType webhookType();
+    @ConfComments({
+        "Supported webhook clients:",
+        "DISCORD"
+    })
+    ClientType client();
 
-    @ConfDefault.DefaultBoolean(false)
+    @ConfDefault.DefaultBoolean(true)
+    @ConfComments("Whether cancelled chat messages should be sent to the webhook.")
     boolean logCancelledMessages();
 
     @SubSection
+    @ConfKey("discord")
+    @ConfComments("Configuration options for the Discord client.")
     DiscordClientConfig discordClientConfig();
 
     interface DiscordClientConfig {
         @ConfDefault.DefaultString("MESSAGE")
+        @ConfComments({
+            "Possible values:",
+            "EMBED_COMPACT, EMBED_PRETTY, MESSAGE"
+        })
         MessageStyle messageStyle();
 
         @ConfDefault.DefaultString("**%1$s > ** %2$s")
+        @ConfComments({
+            "Placeholders:",
+            "%1$s - Player name",
+            "%2$s - Message"
+        })
         String messageFormat();
 
         enum MessageStyle {
-            EMBED_PRETTY, EMBED_COMPACT, MESSAGE
+            EMBED_COMPACT, EMBED_PRETTY, MESSAGE
         }
     }
 }
