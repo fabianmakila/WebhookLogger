f69dc507ca5052496974bb7c8f5afe805eac00de
fabianmakila <contact@fabianadrian.fi>
2023-04-21 13:27:29 +0300

    fix: add proper config annotations

--- src/main/java/fi/fabianadrian/webhookchatlogger/config/WebhookChatLoggerConfig.java
+++ src/main/java/fi/fabianadrian/webhookchatlogger/config/WebhookChatLoggerConfig.java
@@ -2,16 +2,20 @@ package fi.fabianadrian.webhookchatlogger.config;
 
 import fi.fabianadrian.webhookchatlogger.client.ClientType;
 import space.arim.dazzleconf.annote.ConfDefault;
+import space.arim.dazzleconf.annote.SubSection;
 
 
 public interface WebhookChatLoggerConfig {
+    @ConfDefault.DefaultString("")
     String url();
 
     @ConfDefault.DefaultString("DISCORD")
     ClientType webhookType();
 
+    @ConfDefault.DefaultBoolean(false)
     boolean logCancelledMessages();
 
+    @SubSection
     DiscordClientConfig discordClientConfig();
 
     interface DiscordClientConfig {
