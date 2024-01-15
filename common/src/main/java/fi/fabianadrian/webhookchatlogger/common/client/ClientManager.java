6c5b283005ea1239b6f8f5fa87831fe3173b1346
FabianAdrian <contact@fabianadrian.fi>
2024-01-15 19:17:01 +0200

    remove author uuid for now

--- common/src/main/java/fi/fabianadrian/webhookchatlogger/common/client/ClientManager.java
+++ common/src/main/java/fi/fabianadrian/webhookchatlogger/common/client/ClientManager.java
@@ -9,8 +9,6 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 
-import java.util.UUID;
-
 public class ClientManager {
 	private final WebhookChatLogger wcl;
 	private final MiniMessage miniMessage = MiniMessage.miniMessage();
@@ -26,7 +24,6 @@ public class ClientManager {
 	public void log(Message message) {
 		String authorName = message.author().getOrDefault(Identity.NAME, "unknown author");
 		Component authorDisplayName = message.author().getOrDefault(Identity.DISPLAY_NAME, Component.text(authorName));
-		UUID authorUUID = message.author().getOrDefault(Identity.UUID, UUID.fromString(authorName));
 
 		TagResolver miniPlaceholdersResolver = MiniPlaceholders.getAudiencePlaceholders(message.author());
 
@@ -34,7 +31,6 @@ public class ClientManager {
 				this.messageFormat,
 				Placeholder.unparsed("author_name", authorName),
 				Placeholder.component("author_display_name", authorDisplayName),
-				Placeholder.unparsed("author_uuid", authorUUID.toString()),
 				miniPlaceholdersResolver
 		);
 
