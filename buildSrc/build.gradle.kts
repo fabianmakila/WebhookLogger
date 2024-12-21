1b8f447a4b91caea8cde8f6235f76348bfc737a7
Fabian Mäkilä <contact@fabianadrian.fi>
2024-12-21 06:43:09 +0200

    bump spotless

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-	implementation("com.diffplug.spotless:spotless-plugin-gradle:7.0.0.BETA2")
+	implementation("com.diffplug.spotless:spotless-plugin-gradle:7.0.0.BETA4")
 	implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.2")
 }
