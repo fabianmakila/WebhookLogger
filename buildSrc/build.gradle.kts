82b602bc0800860e0d86b781c361d1e8512bb5df
Fabian Mäkilä <contact@fabianadrian.fi>
2025-07-31 22:18:49 +0300

    upgrade the gradle wrapper

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies {
 	implementation("com.diffplug.spotless:spotless-plugin-gradle:7.2.1")
-	implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.5")
+	implementation("com.gradleup.shadow:shadow-gradle-plugin:9.0.0-rc2")
 }
