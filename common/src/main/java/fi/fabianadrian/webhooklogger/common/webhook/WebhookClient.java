5bfde39993e64b2b286aab0a8af1ca639cfcf1bc
Fabian Mäkilä <contact@fabianadrian.fi>
2025-10-02 13:13:56 +0300

    fix webhook sending

--- common/src/main/java/fi/fabianadrian/webhooklogger/common/webhook/WebhookClient.java
+++ common/src/main/java/fi/fabianadrian/webhooklogger/common/webhook/WebhookClient.java
@@ -11,106 +11,108 @@ import net.kyori.adventure.text.serializer.ComponentEncoder;
 import net.kyori.adventure.text.serializer.ansi.ANSIComponentSerializer;
 import net.kyori.ansi.ColorLevel;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringJoiner;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class WebhookClient {
-	private final WebHookClient client;
-	private final WebhookLogger webhookLogger;
-	private final ScheduledFuture<?> scheduledSendMessageTask;
-	private final MainConfig.WebhookConfig config;
-	private final ComponentEncoder<Component, String> serializer;
-	private final Queue<WebHook> webhookQueue = new ArrayDeque<>();
-	private StringJoiner joiner;
-	private int messagesBuffered = 0;
+    private final WebHookClient client;
+    private final WebhookLogger webhookLogger;
+    private final ScheduledFuture<?> scheduledSendMessageTask;
+    private final MainConfig.WebhookConfig config;
+    private final ComponentEncoder<Component, String> serializer;
+    private final Queue<WebHook> webhookQueue = new ArrayDeque<>();
+    private StringJoiner joiner;
+    private int messagesBuffered = 0;
 
-	public WebhookClient(WebhookLogger webhookLogger, MainConfig.WebhookConfig config) {
-		this.webhookLogger = webhookLogger;
-		this.config = config;
-		this.client = WebHookClient.fromURL(config.url());
+    public WebhookClient(WebhookLogger webhookLogger, MainConfig.WebhookConfig config) {
+        this.webhookLogger = webhookLogger;
+        this.config = config;
+        this.client = WebHookClient.fromURL(config.url());
 
-		switch (config.messageStyle()) {
-			case NORMAL -> {
-				DiscordSerializer discordSerializer = new DiscordSerializer();
-				discordSerializer.setDefaultOptions(DiscordSerializerOptions.defaults().withFlattener(webhookLogger.componentFlattener()));
-				this.serializer = discordSerializer;
-				this.joiner = new StringJoiner("\n");
-			}
-			case CODE_BLOCK -> {
-				this.joiner = new StringJoiner("\n", "```ansi\n", "\n```");
-				this.serializer = ANSIComponentSerializer
-						.builder()
-						.colorLevel(ColorLevel.INDEXED_8)
-						.flattener(webhookLogger.componentFlattener())
-						.build();
-			}
-			default -> throw new IllegalStateException("Unknown message style");
-		}
+        switch (config.messageStyle()) {
+            case NORMAL -> {
+                DiscordSerializer discordSerializer = new DiscordSerializer();
+                discordSerializer.setDefaultOptions(DiscordSerializerOptions.defaults().withFlattener(webhookLogger.componentFlattener()));
+                this.serializer = discordSerializer;
+                this.joiner = new StringJoiner("\n");
+            }
+            case CODE_BLOCK -> {
+                this.joiner = new StringJoiner("\n", "```ansi\n", "\n```");
+                this.serializer = ANSIComponentSerializer
+                        .builder()
+                        .colorLevel(ColorLevel.INDEXED_8)
+                        .flattener(webhookLogger.componentFlattener())
+                        .build();
+            }
+            default -> throw new IllegalStateException("Unknown message style");
+        }
 
-		this.scheduledSendMessageTask = webhookLogger.scheduler().scheduleAtFixedRate(
-				() -> {
-					if (this.messagesBuffered < this.config.minimumQueueSize()) {
-						return;
-					}
-					join();
-					send();
-				},
-				0,
-				config.sendRate(),
-				TimeUnit.SECONDS
-		);
-	}
+        this.scheduledSendMessageTask = webhookLogger.scheduler().scheduleAtFixedRate(
+                () -> {
+                    if (this.messagesBuffered >= this.config.minimumQueueSize()) {
+                        join();
+                    }
+                    send();
+                },
+                0,
+                config.sendRate(),
+                TimeUnit.SECONDS
+        );
+    }
 
-	public void add(Component component) {
-		this.webhookLogger.scheduler().schedule(() -> {
-			String serialized = this.serializer.serialize(component);
+    public void add(Component component) {
+        this.webhookLogger.scheduler().schedule(() -> {
+            String serialized = this.serializer.serialize(component);
 
-			for (Map.Entry<Pattern, String> entry : this.webhookLogger.mainConfig().textReplacements().entrySet()) {
-				Matcher matcher = entry.getKey().matcher(serialized);
-				serialized = matcher.replaceAll(entry.getValue());
-			}
+            for (Map.Entry<Pattern, String> entry : this.webhookLogger.mainConfig().textReplacements().entrySet()) {
+                Matcher matcher = entry.getKey().matcher(serialized);
+                serialized = matcher.replaceAll(entry.getValue());
+            }
 
-			if (this.joiner.length() + serialized.length() > 2000) {
-				join();
-			}
+            if (this.joiner.length() + serialized.length() > 2000) {
+                join();
+            }
 
-			this.joiner.add(serialized);
-			this.messagesBuffered++;
-		}, 0, TimeUnit.SECONDS);
-	}
+            this.joiner.add(serialized);
+            this.messagesBuffered++;
+        }, 0, TimeUnit.SECONDS);
+    }
 
-	public void shutdown() {
-		if (this.scheduledSendMessageTask != null) {
-			this.scheduledSendMessageTask.cancel(false);
-		}
-	}
+    public void shutdown() {
+        if (this.scheduledSendMessageTask != null) {
+            this.scheduledSendMessageTask.cancel(false);
+        }
+    }
 
-	private void join() {
-		this.webhookQueue.add(WebHook.builder().content(this.joiner.toString()).build());
-		this.messagesBuffered = 0;
-		switch (config.messageStyle()) {
-			case NORMAL -> this.joiner = new StringJoiner("\n");
-			case CODE_BLOCK -> this.joiner = new StringJoiner("\n", "```ansi\n", "\n```");
-			default -> throw new IllegalStateException("Unknown message style");
-		}
-	}
+    private void join() {
+        this.webhookQueue.add(WebHook.builder().content(this.joiner.toString()).build());
+        this.messagesBuffered = 0;
+        switch (this.config.messageStyle()) {
+            case NORMAL -> this.joiner = new StringJoiner("\n");
+            case CODE_BLOCK -> this.joiner = new StringJoiner("\n", "```ansi\n", "\n```");
+            default -> throw new IllegalStateException("Unknown message style");
+        }
+    }
 
-	private void send() {
-		WebHook webHook = webhookQueue.peek();
-		if (webHook == null) {
-			return;
-		}
-		this.client.sendWebHook(webHook).thenAccept(response -> {
-			switch (response.statusCode()) {
-				case 204 -> webhookQueue.poll();
-				case 429 ->
-						this.webhookLogger.logger().warn("Failed to send a webhook to {} due to rate limit. Consider increasing the sendRate in the configuration to avoid this", this.config.url());
-				default ->
-						this.webhookLogger.logger().warn("Failed to send a webhook to {}. Got status code {}", this.config.url(), response.statusCode());
-			}
-		});
-	}
+    private void send() {
+        WebHook webHook = this.webhookQueue.peek();
+        if (webHook == null) {
+            return;
+        }
+        this.client.sendWebHook(webHook).thenAccept(response -> {
+            switch (response.statusCode()) {
+                case 204 -> this.webhookQueue.poll();
+                case 429 ->
+                        this.webhookLogger.logger().warn("Failed to send a webhook to {} due to rate limit. Consider increasing the sendRate in the configuration to avoid this", this.config.url());
+                default ->
+                        this.webhookLogger.logger().warn("Failed to send a webhook to {}. Got status code {}", this.config.url(), response.statusCode());
+            }
+        });
+    }
 }
